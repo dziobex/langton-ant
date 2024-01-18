014e20b7cdaea52ab879b89e3bf715f1197a1120
dziobex <58388062+dziobex@users.noreply.github.com>
2024-01-18 08:57:25 +0100

    Dodanie polecenia, podpisu do readme i pliku ze stałymi

---
+++ constants.h
@@ -0,0 +1,22 @@
+#ifndef _CONSTANTS_H_
+#define _CONSTANTS_H_
+
+// stałe dla programu
+
+// maksymalne rozmiary siatki (wiersze, kolumny)
+#define M_MAX 1000
+#define N_MAX 1000
+
+// znaki specjalne
+#define WHITE " "
+#define BLACK "█"
+#define WHITE_UP "△"
+#define BLACK_UP "▲"
+#define WHITE_RIGHT "▷"
+#define BLACK_RIGHT "▶"
+#define WHITE_DOWN "▽"
+#define BLACK_DOWN "▼"
+#define WHITE_LEFT "◁"
+#define BLACK_LEFT "◀"
+
+#endif
